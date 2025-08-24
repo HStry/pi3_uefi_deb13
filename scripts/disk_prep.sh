6ea874afdc1ff0daa4222df8f30a42637b2e1081
Hans Strijker <45356184+HStry@users.noreply.github.com>
2025-08-24 21:43:32 +0200

    Update and rename deploy_disk_scaffold.sh to disk_prep.sh

--- scripts/disk_prep.sh
+++ scripts/disk_prep.sh
@@ -21,6 +21,64 @@ default_lv_parts() (
   echo 'tmp,          1G, /tmp,           ext4'
 )
 
+help_msg="$(cat << EOF
+SYNOPSIS:
+  ${_file} [OPTIONS] DEVICE [DEVICE...]
+
+DESCRIPTION:
+  This script partitions an SD card for your Raspberry Pi, and copies the
+  firmware and resources from efi.resources onto your EFI partition. You can use
+  a Debian 13 (Trixie) image on USB to install your raspberry onto this SD card.
+  Both the netinst and DVD-1 images work, though the DVD-1 variant is faster
+  during install. Ventoy does not work consistently. It often (though not
+  always) will ask to mount the install media again during install.
+  
+  Go through the install process. Use the partitions on the disk during install,
+  no need to format them. Before rebooting, use the second terminal accessible
+  through [CTRL]+[ALT]+[F2] and run '/target/boot/efi/setup'
+  
+
+OPTIONS:
+  -h, --help                     Show this help text.
+  -F, --force                    Do not ask for confirmation. Don't use this.
+      --wipe-disk                Wipe (zero-fill) the disk.
+  -d, --device     BLOCKDEV  *R  Block device to prep for installation.
+  -p, --partition  PARTSPEC   R  Partition specification. See below.
+  -v, --vg-name    VG_NAME       Name for the generated lvm volume group.
+  -p, --lv-part    LV_SPEC    R  LV partition specification. See below.
+  -f, --firmware   FIRMWARE  *   RPi Firmware (https://github.com/pftf/RPi3).
+
+  Asterisks indicate required variables. R indicates repeatable variables.
+
+BLOCKDEV:
+  Disk to reformat, and deploy the resources onto. The contents on this disk
+  WILL be destroyed.
+
+PARTSPEC:
+  'name,size,type,mountpoint,fstype'
+  If provided, only these partitions will be created. Tread carefully.
+  By default, it will create:
+$(default_parts | while IFS= read line; do echo "    ${line}"; done)
+  
+LV_SPEC:
+  'lv_name,lv_size,mountpoint,fstype'
+  If provided, only those partitions will be created, so this might break your
+  setup if, e.g. you forget to add a root partition.
+  By default, it will create:
+$(default_lv_parts | while IFS= read line; do echo "    ${line}"; done)
+
+VG_NAME:
+  A valid volume group name. If left omitted, it will generate a name from the
+  UTC datetime as 'sys%02d%06x' whereby '%02d' will be the last two digits of
+  the year, and %06x the hex value of the seconds since the year's start,
+  divided by two.
+
+FIRMWARE:
+  Must point to a firmware file. There is a very limited form of autolocate
+  if no firmware is provided, but ymmv.
+EOF
+)"
+
 parts() (
   if [ -n "${parts}" ]; then
     echo "${parts}" | sed -e 's/;/\n/g'
@@ -64,8 +122,6 @@ vg_name() (
   s="$(( ($(date -d "${dt}" +%s) - $(date -d "${d0}" +%s)) / 2 ))"
   printf 'sys%02d%06x' "$(date -d "${dt}" +%y)" "${s}"
 )
-
-
 
 help=0
 force=0
@@ -121,32 +177,8 @@ for arg in "$@"; do
   exit 1;;
 done
 
-[ $# -eq 0 ] || [ ${help} -gt 0 ] && cat << EOF >&2
-USAGE:
-  ${_file} [OPTIONS] DEVICE [DEVICE...]
-
-OPTIONS:
-  -h, --help                   Show this help text.
-  -F, --force                  Do not ask for confirmation. Don't use this.
-  -d, --device     BLOCKDEV  * Block device to prep for installation.
-  -p, --partition  PARTSPEC    Partition specification. See below.
-  -v, --vg-name    VG_NAME     Name for the generated lvm volume group.
-  -p, --lv-part    LV_SPEC     LV partition specification. See below.
-  -f, --firmware   FILE      * RPi Firmware (https://github.com/pftf/RPi3).
-
-  Asterisks indicate required variables.
-
-PARTSPEC:
-  'name,size,type,mountpoint,fstype'
-  If provided, only these partitions will be created. Tread carefully.
-  By default, it will create:
-$(default_parts | while IFS= read line; do echo "    ${line}"; done)
-  
-LV_SPEC:
-  'lv_name,lv_size,mountpoint,fstype'
-  If provided, only those partitions will be created, so this might break your
-  setup if, e.g. you forget to add a root partition.
-  By default, it will create:
-$(default_lv_parts | while IFS= read line; do echo "    ${line}"; done)
-EOF
+if [ $# -eq 0 ] || [ ${help} -gt 0 ]; then
+  echo "${help_msg}"
+  exit
+fi
 
